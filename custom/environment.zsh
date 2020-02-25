cc49b7a32f131bf19c76eefcb3e89d6160ddc17c
Greg Cook <5386965+greg-cook@users.noreply.github.com>
2020-02-26 10:42:54 +1100

    update environment settings

--- custom/environment.zsh
+++ custom/environment.zsh
@@ -2,12 +2,17 @@
 export GOPATH="${HOME}/go"
 
 # Add directories to path to prefer these over system binaries
-export PATH="$HOME/bin:/usr/local/bin:$PATH"
-export PATH="/usr/local/opt/helm@2/bin:$PATH"
 export PATH="/usr/local/opt/libpq/bin:$PATH"
 export PATH="/usr/local/opt/mysql-client@5.7/bin:$PATH"
 export PATH="/usr/local/opt/openssl/bin:$PATH"
 export PATH="/usr/local/opt/v8@3.15/bin:$PATH"
 
+# Set editors
 export EDITOR='nvim'
-export VISUAL_EDITOR='code'
+export VISUAL='nvim'
+
+# Suppress direnv noise
+export DIRENV_LOG_FORMAT=""
+
+# Don't prompt for .env loading
+export ZSH_DOTENV_PROMPT=false
