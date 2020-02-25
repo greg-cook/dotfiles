5ff4aad75912021601a1e32cbb3c7a983be119bb
Greg Cook <5386965+greg-cook@users.noreply.github.com>
2020-02-26 10:58:12 +1100

    Cleanup functions, add local branch function

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -1,26 +1,22 @@
-function old-kube {
-  if [[ $(which kubectl) = "/usr/local/bin/kubectl" ]]; then
-    mv $HOME/bin/kubectl* $HOME/bin/kubectl
-    src
-  fi
-}
-
-function new-kube {
-  if [[ $(which kubectl) = "$HOME/bin/kubectl" ]]; then
-    mv $HOME/bin/kubectl $HOME/bin/kubectl.old
-    src
-  fi
-}
-
-
-# Clean deleted upstream branches
-function git-clean {
+function fetch-and-prune {
   # fetch all branches
   git fetch --all
   # Prune remote branches
   git remote prune origin
+}
+
+# Clean deleted upstream branches
+function git-clean {
+  fetch-and-prune
   # Delete local branches that are ":gone" from origin
   git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D
+}
+
+# List branches that only exist locally and not on remote
+function local-branches {
+  fetch-and-prune
+  # Show local branches
+  git branch -vv | cut -c 3- | awk '$3 !~/\[/ { print $1 }'
 }
 
 # Clean repos recursively
@@ -33,4 +29,32 @@ function cleanr {
     git-clean
     cd ..
   done
+}
+
+# List local-only branches recursively
+# Assumes all your repos are in a single folder
+# Relies on git-clean function
+function branchr {
+  for dir in */; do
+  echo "<--- Cleaning local branches in repo $dir --->"
+    cd $dir
+    local-branches
+    cd ..
+  done
+}
+
+# Intercept well-intentioned brew commands that will break things
+function brew() {
+  case $@ in
+    *elasticsearch|*mysql|*node|*nodenv|*nvm|*postgresql|*rbenv|*ruby|*rvm|*yarn)
+      if [[ $1 == "install" || $1 == "upgrade" ]]; then
+        echo "Here be dragons, you don't need to do this, ask someone for guidance..."
+      else
+        command brew $@
+      fi
+      ;;
+    *)
+      command brew $@
+      ;;
+  esac
 }
